ecb4a82ab136e9e46cdf49cc53f0450d9f86c4de
MartiVallhonrat <martivallhonrat@gmail.com>
2023-04-11 00:02:09 +0200

    new project StarWars

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
+import { StarshipsComponent } from './starships/starships.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  {path: "", redirectTo: '/home', pathMatch: 'full'},
+  {path: "home", component: HomeComponent},
+  {path: "starships", component: StarshipsComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    StarshipsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
